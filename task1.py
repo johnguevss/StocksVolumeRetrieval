015e209deff96a03a89eef8a25bebde1be49d490
johnguevss <jrguevarra.21@gmail.com>
2023-02-06 10:16:04 +0800

    minor adjustments for task 1 and task 3
    -changed task 1 from ipynb to py file
    -converted to uppercase sql commands

---
+++ task1.py
@@ -0,0 +1,51 @@
+import requests
+import os
+import pandas as pd
+import datetime
+
+# set parameter values
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+ALPHAVANTAGE_KEY = os.environ.get("ALPHA_KEY")
+STOCK = "TSLA"
+YEAR_THRESHOLD = 4
+
+# TODO 1: Connect to alphavantage API and retrieve stock data
+alpha_param = {
+    "function": "TIME_SERIES_WEEKLY",
+    "symbol": STOCK,
+    "apikey": ALPHAVANTAGE_KEY,
+    "datatype": "json"
+}
+
+response = requests.get(STOCK_ENDPOINT, params=alpha_param)
+data = response.json()
+
+# TODO 2: From the response, create a dataframe that contains year and volume
+weekly_df = pd.DataFrame.from_dict(data['Weekly Time Series'],
+                                   orient='index',
+                                   columns=['5. volume']
+                                   )
+
+clean_weekly_df = weekly_df.reset_index(names='date') \
+    .rename(columns={'5. volume': 'volume'})
+
+# add year column
+clean_weekly_df['YEAR'] = clean_weekly_df['date'].str[0:4] \
+    .astype(int)
+# set datatype
+clean_weekly_df['volume'] = clean_weekly_df['volume'].astype(float)
+
+# TODO 3: Get the average volume per year for the past 5 years for the selected stock
+
+# compute past 5 years
+year_filter = int(datetime.date.today().year) - YEAR_THRESHOLD
+
+# filter, aggregate and fix schema
+vol_by_year_df = clean_weekly_df.query(f'YEAR >= {year_filter}') \
+    .groupby('YEAR').mean() \
+    .rename(columns={"volume": "AVERAGE_VOLUME"}) \
+    .assign(STOCK=STOCK) \
+    .reset_index()
+
+# TODO 4:output the df into a csv file with columns STOCK, YEAR, AVERAGE_VOLUME
+vol_by_year_df[['STOCK', 'YEAR', 'AVERAGE_VOLUME']].to_csv(f"{STOCK}_average_vol_per_year.csv", index=False)
--- task3.sql
+++ task3.sql
@@ -1,4 +1,5 @@
---For each product category, which month/s do they have a spike or dip in sales? So that the retailer can order additional units for categories that are in demand next month, and less stocks for unpopular ones. especially for categories with short shelflife. They can also use this information to plan promotions.
+-- For each product category, which month/s do they have a spike or dip in sales? So that the retailer can order additional units for categories that are in demand next month, 
+-- and less stocks for unpopular ones. especially for categories with short shelflife. They can also use this information to plan promotions.
 
 SELECT 
 	substr(ord.orderDate,1,4) as year, 
@@ -6,11 +7,11 @@ SELECT
     cat.categoryName,
     sum(od.quantity)
 FROM [Orders] ord
-left join orderDetails od
-	on od.orderId = ord.orderId
-left join products pr
-	on pr.productid = od.productid
-left join categories cat
-	on cat.categoryId = pr.categoryId
-group by year, month, categoryName
-order by categoryName
+LEFT JOIN orderDetails od
+	ON od.orderId = ord.orderId
+LEFT JOIN products pr
+	ON pr.productid = od.productid
+LEFT JOIN categories cat
+	ON cat.categoryId = pr.categoryId
+GROUP BY year, month, categoryName
+ORDER BY categoryName
